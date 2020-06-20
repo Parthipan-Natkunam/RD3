b2ce9d8510ec86352041d7349b50021b7c67b88a
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2020-06-20 21:06:53 +0530

    add proptypes validator to chartWrapper component

--- src/components/ChartWrapper.jsx
+++ src/components/ChartWrapper.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from "react";
+import PropTypes from "prop-types";
 import { D3Chart } from "./D3Chart";
 
 const ChartWrapper = ({ chartId }) => {
@@ -9,6 +10,10 @@ const ChartWrapper = ({ chartId }) => {
   }, []);
 
   return <div id={chartId} ref={chartRef}></div>;
+};
+
+ChartWrapper.propTypes = {
+  chartId: PropTypes.string.isRequired,
 };
 
 export default ChartWrapper;
