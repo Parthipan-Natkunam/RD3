fe632aaed2c73eac559d4b6ddd5709a0f17e1eae
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2020-06-21 23:41:25 +0530

    use evergreen-ui panes for divs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { Pane, Heading } from "evergreen-ui";
 import ChartWrapper from "./components/ChartWrapper";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <ChartWrapper chartId={"test-chart"} />
-    </div>
+    <Pane
+      background="greenTint"
+      width="100vw"
+      height="100vh"
+      padding={16}
+      paddingTop={0}
+    >
+      <Pane
+        display="flex"
+        background="tint2"
+        borderRadius={3}
+        marginBottom={16}
+        paddingTop={16}
+        paddingBottom={16}
+      >
+        <Pane flex={1} alignItems="center" display="flex">
+          <Heading size={600}>Gilead Stats</Heading>
+        </Pane>
+      </Pane>
+      <Pane marginTop={16} marginBottom={16}>
+        <ChartWrapper chartId={"test-chart"} />
+      </Pane>
+    </Pane>
   );
 }
 
